9fe66efc0c2160d5dfa3a325c0345bf693b73063
Federico Damián Schonborn <fdschonborn@gmail.com>
2022-06-05 03:48:00 -0300

    encoding: Add Marshal functions
    Signed-off-by: Federico Damián Schonborn <fdschonborn@gmail.com>

--- encoding/encoding.go
+++ encoding/encoding.go
@@ -2,17 +2,37 @@ package encoding
 
 type UnmarshalFunc func([]byte, any) error
 
-func Unmarshal[T any](f UnmarshalFunc, data []byte) (value T, err error) {
-	err = f(data, &value)
+func Unmarshal[T any](f UnmarshalFunc, data []byte) (T, error) {
+	var value T
+	err := f(data, &value)
 	return value, err
 }
 
-func UnmarshalSlice[T any, S ~[]T](f UnmarshalFunc, data []byte) (value S, err error) {
-	err = f(data, &value)
+func UnmarshalSlice[T any, S ~[]T](f UnmarshalFunc, data []byte) (S, error) {
+	var value S
+	err := f(data, &value)
 	return value, err
 }
 
-func UnmarshalMap[K comparable, V any, M ~map[K]V](f UnmarshalFunc, data []byte) (value M, err error) {
-	err = f(data, &value)
+func UnmarshalMap[K comparable, V any, M ~map[K]V](f UnmarshalFunc, data []byte) (M, error) {
+	var value M
+	err := f(data, &value)
 	return value, err
+}
+
+type MarshalFunc func(any) ([]byte, error)
+
+func Marshal[T any](f MarshalFunc, value T) ([]byte, error) {
+	data, err := f(value)
+	return data, err
+}
+
+func MarshalSlice[T any, S ~[]T](f MarshalFunc, value S) ([]byte, error) {
+	data, err := f(value)
+	return data, err
+}
+
+func MarshalMap[K comparable, V any, M ~map[K]V](f MarshalFunc, value M) ([]byte, error) {
+	data, err := f(value)
+	return data, err
 }
